18b3cda49f899b55ef225fa6d954b01eb5310ac0
Nicholas Donnelly <nick@donnelly.cc>
2018-07-25 14:19:43 +0200

    Ability to stitch images onto one another

--- deuxmasbot/image/stitcher.py
+++ deuxmasbot/image/stitcher.py
@@ -1,4 +1,12 @@
+from enum import Enum
+
 from wand.image import Image
+
+class ImageSide(Enum):
+    Top = 0
+    Right = 1
+    Bottom = 2
+    Left = 4
 
 class ImageStitcher:
    
@@ -6,3 +14,17 @@ class ImageStitcher:
         self.image = Image(filename=base_image)
         self.coords = sprite_coordinates
 
+    def get_dimensions(self):
+        return self.image.size
+
+    def update_image(self, img):
+        self.image = img
+        self.width = img.width
+        self.height = img.height
+
+    def draw_sprite(self, sprite):
+        x, y = self.coords[0]
+        del self.coords[0]
+        new_image = self.image.clone() 
+        new_image.watermark(image=sprite, transparency=0.0, left=x, top=y)
+        self.update_image(new_image)
--- tests/test_image.py
+++ tests/test_image.py
@@ -10,4 +10,17 @@ def test_assigns_image_on_init():
     the_stitcher = stitcher.ImageStitcher('images/stocking-base.png', [])
     assert isinstance(the_stitcher.image, wand.image.Image)
 
+def test_update_image():
+    the_stitcher = stitcher.ImageStitcher('images/stocking-base.png', [])
+    img = wand.image.Image(width=50,height=50)
+    the_stitcher.update_image(img)
 
+    assert the_stitcher.width is 50 and the_stitcher.height is 50 and the_stitcher.image is img
+    
+def test_draw_one_sprite():
+    the_stitcher = stitcher.ImageStitcher('images/stocking-base.png', [(10, 10)])
+    prev_img = the_stitcher.image
+    test_sprite = wand.image.Image(filename='images/test-sprite.png')
+    the_stitcher.draw_sprite(test_sprite)
+
+    assert the_stitcher.image is not prev_img
