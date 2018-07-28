bef2439260f60191e6a2b685746d67c967a90105
Nicholas Donnelly <nick@donnelly.cc>
2018-07-28 10:32:53 +0200

    Add facility to position sprite at time of draw, remove dead code

--- deuxmasbot/image/stitcher.py
+++ deuxmasbot/image/stitcher.py
@@ -4,15 +4,9 @@ from wand.image import Image
 from wand.color import Color
 from wand.drawing import Drawing
 
-class ImageSide(Enum):
-    Top = 0
-    Right = 1
-    Bottom = 2
-    Left = 4
-
 class ImageStitcher:
    
-    def __init__(self, base_image, sprite_coordinates):
+    def __init__(self, base_image, sprite_coordinates = []):
         self.image = Image(filename=base_image, background=Color("transparent"))
         self.coords = sprite_coordinates
 
@@ -24,22 +18,39 @@ class ImageStitcher:
         self.width = img.width
         self.height = img.height
 
-    def draw_sprite(self, sprite, rounded=True):
+    def draw_sprite(self, sprite, position=-1, rounded=True):
         sprite.alpha_channel = True
         if rounded:
             with Image(width=sprite.width, height=sprite.height, 
                        background=Color('transparent')) as mask:
                 with Drawing() as ctx:
-                    ctx.fill_color = Color("black")
-                    ctx.rectangle(left=0, top=0, 
-                                  width=mask.width, height=mask.height, radius=mask.width / 2)
-                    ctx(mask)
+                    if rounded:
+                        ctx.fill_color = Color("black")
+                        ctx.rectangle(left=0, top=0, 
+                                      width=mask.width, height=mask.height, radius=mask.width / 2)
+                        ctx(mask)
                     sprite.composite_channel('all_channels', mask, 'screen')
                     mask.negate()
                     sprite.composite_channel('alpha', mask, 'copy_opacity')
 
-        x, y = self.coords[0]
-        del self.coords[0]
+        x, y = self.__get_coordinate(position)
         new_image = self.image.clone() 
         new_image.watermark(image=sprite, transparency=0.0, left=x, top=y)
         self.update_image(new_image)
+
+    def __get_coordinate(self, position):
+        if len(self.coords) is 0 and position == -1:
+            raise InvalidCoordinateError('No coordinates provided and none left in initializer')
+
+        if position == -1:
+            x, y = self.coords[0]
+            del self.coords[0]
+        else:
+            x, y = position
+
+        return x, y
+
+
+class InvalidCoordinateError(Exception):
+    def __init__(self, *args, **kwargs):
+        Exception.__init__(self, *args, *kwargs)
--- tests/test_image.py
+++ tests/test_image.py
@@ -2,8 +2,10 @@ import sys, os
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 
 import wand
+import pytest
 
-from deuxmasbot.image import stitcher as stitcher
+from deuxmasbot.image import stitcher
+from deuxmasbot.image.stitcher import InvalidCoordinateError
 
 # ImageStitcher
 def test_assigns_image_on_init():
@@ -24,3 +26,19 @@ def test_draw_one_sprite():
     the_stitcher.draw_sprite(test_sprite)
 
     assert the_stitcher.image is not prev_img
+
+def test_draw_given_coordinate():
+    the_stitcher = stitcher.ImageStitcher('images/stocking-base.png')
+    prev_img = the_stitcher.image
+    test_sprite = wand.image.Image(filename='images/test-sprite.png')
+    the_stitcher.draw_sprite(test_sprite, (10, 10))
+
+    assert the_stitcher.image is not prev_img
+
+def test_raises_on_bad_coordinate():
+    the_stitcher = stitcher.ImageStitcher('images/stocking-base.png')
+    prev_img = the_stitcher.image
+    test_sprite = wand.image.Image(filename='images/test-sprite.png')
+
+    with pytest.raises(InvalidCoordinateError):
+        the_stitcher.draw_sprite(test_sprite)
